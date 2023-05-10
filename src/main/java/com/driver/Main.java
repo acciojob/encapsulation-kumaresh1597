13850e6f7f93423da4b1e53335a6a9df7d1d4d07
kumaresh1597 <kumaresh.1597@gmail.com>
2023-05-10 14:49:51 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
-  
+    // Task 2
+    public static void main(String[] args) {
+//        Task 3
+        RWOnly obj = new RWOnly();
+//       Task - 4
+//        obj.name = "kumar";
+//        System.out.println(obj.name);
+
+//        java: name has private access in com.driver.RWOnly
+//         the above two lines of code throws error, because name in RWOnly class is privavte
+//         private member cannot be accessed directly through obj
+
+//        Task 6
+//        setting a value to name in Main function using setter function
+//        and access it's value using getter function
+          obj.setName("Akash");
+          String name = obj.getName();
+
+          System.out.println(name);
+          // OUTPUT
+//        classpath C:\Users\HANIFA\IdeaProjects\encapsulation-kumaresh1597\target\classes com.driver.Main
+//                Akash
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    // Task 1
+    private String name;
+
+    // Task 5
+    // implementing getter and setters member function in class RWOnly
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
